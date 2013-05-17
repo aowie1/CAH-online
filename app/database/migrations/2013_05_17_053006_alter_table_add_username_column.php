0fc012f04f8d9a6e6122881314b34e996dc54a6d
Heath Naylor <HeathNaylor@gmail.com>
2013-05-16 23:58:31 -0600

    Add username column to users table.

---
+++ app/database/migrations/2013_05_17_053006_alter_table_add_username_column.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableAddUsernameColumn extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function($table){
+			$table->string('username')->unique();
+		});
+		DB::statement('ALTER TABLE users MODIFY username varchar(255) AFTER id;');
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function($table){
+			$table->dropColumn('username');
+		});
+	}
+
+}
